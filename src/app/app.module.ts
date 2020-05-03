e696ec41ed29e7cfa3b1c6b67381ebae20f04724
Maati <douwah95@gmail.com>
2020-05-03 05:29:01 +0000

    add c/cl/s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddClientComponent } from './client/add-client/add-client.component';
+import { ListClientComponent } from './client/list-client/list-client.component';
+import { AddCategorieComponent } from './categorie/add-categorie/add-categorie.component';
+import { ListCategorieComponent } from './categorie/list-categorie/list-categorie.component';
+import { ListScategorieComponent } from './scategorie/list-scategorie/list-scategorie.component';
+import { AddScategorieComponent } from './scategorie/add-scategorie/add-scategorie.component';
+import { AddArticleComponent } from './article/add-article/add-article.component';
+import { ListArticleComponent } from './article/list-article/list-article.component';
+import { ListFournisseurComponent } from './fournisseur/list-fournisseur/list-fournisseur.component';
+import { AddFournisseurComponent } from './fournisseur/add-fournisseur/add-fournisseur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddClientComponent,
+    ListClientComponent,
+    AddCategorieComponent,
+    ListCategorieComponent,
+    ListScategorieComponent,
+    AddScategorieComponent,
+    AddArticleComponent,
+    ListArticleComponent,
+    ListFournisseurComponent,
+    AddFournisseurComponent
   ],
   imports: [
     BrowserModule
